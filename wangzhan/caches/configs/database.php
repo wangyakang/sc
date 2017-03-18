459b34db32c20a60b900fb75b5088ceea0d684f4
wangyakang <1967194353@qq.com>
2017-03-18 10:19:02 +0800

    first

---
+++ wangzhan/caches/configs/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => 'sqld.duapp.com',
+		'port' => 4050,
+		'database' => 'itQkYyuOwxqFlZLHulsb',
+		'username' => '1250de1ae4c64a30ab3b2c43b3a97480',
+		'password' => 'a021088022984534a76974f520675d10',
+		'tablepre' => 'v9_',
+		'charset' => 'utf8',
+		'type' => 'mysqli',
+		'debug' => true,
+		'pconnect' => 0,
+		'autoconnect' => 0
+		),
+);
+
+?>
